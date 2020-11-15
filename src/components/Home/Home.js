f21a5d4dcc7092acc39a7bd4c379d933ba0f34b6
ChingHuiHui <sunny@3drens.com>
2020-11-15 10:11:00 +0800

    UI

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet, TouchableOpacity  } from 'react-native'
 import { Container, Content, Button, Icon } from 'native-base'
 import Header from '../UI/Header'
 
@@ -33,10 +33,119 @@ function Home(props) {
     <Container>
       <Header/>
       <Content>
-        <Text>HOME</Text>
+        <View style={styles.banner}>
+        </View>
+        <View style={styles.brand}>
+          <Text style={styles.brandTitle}>HUI RENT</Text>
+          <Text style={styles.brandTitle}>HUI RENT</Text>
+          <Text style={styles.brandTitle}>HUI RENT</Text>
+        </View>
+        <TouchableOpacity>
+            <ImageBackground
+              imageStyle={{ borderRadius: 6}}
+              style={styles.image}
+              source={{uri: 'https://ysolife.com/wp-content/uploads/2017/11/06-%E4%B8%AD%E7%B7%B4-min.jpg'}}
+            >
+              <View style={styles.filter}/>
+              {/* <View style={styles.roomInfo}>
+              </View> */}
+                <Text style={styles.roomPrice}>480 / h</Text>
+                <Text style={styles.roomName}>大展演廳</Text>
+            </ImageBackground>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <ImageBackground
+            imageStyle={{ borderRadius: 6}}
+            style={styles.image}
+            source={{uri: 'http://www.irockmusic.com.tw/wp-content/uploads/2019/11/IMG_0761-1024x683.jpg'}}
+          >
+            <View style={styles.filter}/>
+              <Text style={styles.roomPrice}>300 / h</Text>
+              <Text style={styles.roomName}>大練團室</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <ImageBackground
+            imageStyle={{ borderRadius: 6}}
+            style={styles.image}
+            source={{uri: 'http://www.irockmusic.com.tw/wp-content/uploads/2018/07/%E5%9C%98%E5%AE%A4%E5%87%BA%E7%A7%9F.jpg'}}
+          >
+            <View style={styles.filter}/>
+              <Text style={styles.roomPrice}>200 / h</Text>
+              <Text style={styles.roomName}>小練團室</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <ImageBackground
+            imageStyle={{ borderRadius: 6}}
+            style={styles.image}
+            source={{uri: 'https://img.ruten.com.tw/s2/7/f7/f2/21729744938994_881.JPG'}}
+          >
+            <View style={styles.filter}/>
+              <Text style={styles.roomPrice}>50 / h</Text>
+              <Text style={styles.roomName}>練鼓室</Text>
+          </ImageBackground>
+        </TouchableOpacity>
       </Content>
     </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  banner: {
+    height: 200,
+    backgroundColor: '#ccc'
+  },
+  brand: {
+    flexDirection : 'row',
+    marginVertical: 32,
+  },
+  brandTitle: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    transform: ([{ rotate: '-15deg' }])
+  },
+  image: {
+    height: 150,
+    marginHorizontal: 16,
+    marginVertical: 8,
+    resizeMode: "cover",
+    justifyContent: "center",
+    position: 'relative',
+    shadowColor: '#000000',
+    shadowOpacity: 0.4,
+    shadowRadius: 1,
+    shadowOffset: {
+      height: 2,
+      width: 1,
+    },
+  },
+  filter: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(0,0,0,.6)',
+    borderRadius: 6
+  },
+  roomName: {
+    color: '#fff',
+    backgroundColor: 'rgba(0,0,0,.6)',
+    textAlign: 'center',
+    paddingVertical: 8,
+    marginBottom: 32
+  },
+  roomPrice: {
+    fontSize: 42,
+    opacity: 0.6,
+    fontWeight: 'bold',
+    color: '#ccc',
+    position:'absolute',
+    right: 16,
+    bottom: 0
+  }
+})
 
 export default Home
