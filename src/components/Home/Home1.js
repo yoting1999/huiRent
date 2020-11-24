c125f3292252080e2b05e3bf43f25cee77de1a3c
ChingHuiHui <sunny@3drens.com>
2020-11-24 13:45:56 +0800

    INSTALL react-native-modal && room's detail modal UI

--- src/components/Home/Home1.js
+++ src/components/Home/Home1.js
@@ -3,7 +3,7 @@ import { View, StyleSheet, Dimensions,Text, TouchableOpacity } from 'react-nativ
 import { ScrollView } from 'react-native-gesture-handler'
 
 import Colors from '../../styles/Colors'
-import Header from '../UI/Header'
+// import Header from '../UI/Header'
 import Slide, { SLIDE_HEIGHT } from '../UI/Slide'
 
 const { width } = Dimensions.get("window")
@@ -11,24 +11,50 @@ const { width } = Dimensions.get("window")
 
 const dummyDATA = [
   {
-    name: '展演廳',
+    name: '大練團室',
     price: 400,
-    uri: 'https://ysolife.com/wp-content/uploads/2017/11/06-%E4%B8%AD%E7%B7%B4-min.jpg'
+    uri: 'https://ysolife.com/wp-content/uploads/2017/11/05-%E5%A4%A7%E7%B7%B4-min-768x510.jpg',
+    equipments: [
+      {type: '基本' ,equipment: ['麥克風','麥克風接線','鼓組','mixer'] },
+      {type: '吉他音箱', equipment: ['Marshall DSL100', 'MX412'] },
+      {type: 'Bass音箱', equipment: ['Little Mark 250 15th Anniversary', 'Traveler'] },
+      {type: '監聽', equipment: ['Superlux SF12'] },
+    ]
   },
   {
-    name: '中練',
+    name: '中練團室',
     price: 300,
-    uri: 'http://www.irockmusic.com.tw/wp-content/uploads/2019/11/IMG_0761-1024x683.jpg'
+    uri: 'http://www.irockmusic.com.tw/wp-content/uploads/2019/11/IMG_0761-1024x683.jpg',
+    equipments: [
+      {type: '基本' ,equipment: ['麥克風','麥克風接線','鼓組','mixer'] },
+      {type: '吉他音箱', equipment: ['VOX 真空管100w','Marshall 100w'] },
+      {type: 'Bass音箱', equipment: ['Ampeg 115w'] },
+      {type: 'KB音箱', equipment: ['Peavey 300w'] },
+      {type: '監聽', equipment: ['Lem k6 300w'] },
+    ]
   },
   {
-    name: '小練',
+    name: '小練團室',
     price: 200,
-    uri: 'http://www.irockmusic.com.tw/wp-content/uploads/2018/07/%E5%9C%98%E5%AE%A4%E5%87%BA%E7%A7%9F.jpg'
+    uri: 'http://www.irockmusic.com.tw/wp-content/uploads/2018/07/%E5%9C%98%E5%AE%A4%E5%87%BA%E7%A7%9F.jpg',
+    equipments: [
+      {type: '基本' ,equipment: ['麥克風','麥克風接線','鼓組','mixer'] },
+      {type: '吉他音箱', equipment: ['VOX 真空管100w','Marshall 100w'] },
+      {type: 'Bass音箱', equipment: ['Fender 100w'] },
+      {type: 'KB音箱', equipment: ['Peavey 300w'] },
+      {type: '監聽', equipment: ['Lem k6 300w', 'Lem k3 350w'] },
+    ]
   },
   {
     name: '鼓室',
     price: 50,
-    uri:   'https://img.ruten.com.tw/s2/7/f7/f2/21729744938994_881.JPG'
+    uri:   'https://img.ruten.com.tw/s2/7/f7/f2/21729744938994_881.JPG',
+    equipments: [
+      {type: '基本' ,equipment: ['麥克風','麥克風接線','鼓組','mixer'] },
+      {type: '吉他音箱', equipment: ['Ampeg 115w'] },
+      {type: 'KB音箱', equipment: ['Peavey 300w'] },
+      {type: '監聽', equipment: ['Lem k6 300w'] },
+    ]
   }
 ]
 
@@ -63,12 +89,9 @@ function Home() {
           onMomentumScrollEnd={handleScroll}
           >
           { dummyDATA && dummyDATA.map((item, index)=> {
-            return <Slide bgImg={item.uri} key={`room-${index+1}`} />
+            return <Slide data={item} bgImg={item.uri} key={`room-${index+1}`} />
           }) }
-          {/* <Slide label="展演廳" bgImg={IMG_DATA[0]} price="480"/>
-          <Slide label="大練團室" bgImg={IMG_DATA[1]} price="300" right/>
-          <Slide label="小練團室" bgImg={IMG_DATA[2]} price="200" />
-          <Slide label="鼓室" bgImg={IMG_DATA[3]} right price="50" /> */}
+
         </ScrollView>
         <View style={styles.titleContainer}>
           <Text style={styles.label}>{ dummyDATA[currentIndex].name }</Text>
