378655076e5ab0ae9149b69c8550e428b1b4c848
RayLin9981 <a259108736@gmail.com>
2020-11-24 15:01:07 +0800

    GiftIndex

--- src/components/Gift/Gift_index.js
+++ src/components/Gift/Gift_index.js
@@ -2,6 +2,8 @@ import React,{useEffect,useState} from 'react'
 import {View,Text} from 'react-native'
 import { Header, Left, Body, Right, Container, Content, Thumbnail, Button, Icon } from 'native-base'
 
+import Header from '../'
+
 function Gift_index() {
 
 
