f5847f849d5420cfa73287cfc9c8b5abdacb257a
RayLin9981 <a259108736@gmail.com>
2020-11-25 15:10:22 +0800

    gift1125v1

--- src/components/Gift/Gift_index.js
+++ src/components/Gift/Gift_index.js
@@ -1,26 +1,28 @@
 import React,{useEffect,useState} from 'react'
-import {View,Text,FlatList} from 'react-native'
+import {View,Text,FlatList,StyleSheet} from 'react-native'
 import { Container, Content, Thumbnail, Button, Icon } from 'native-base'
 import Header from '../UI/Header'
 import Colors from '../../styles/Colors'
 
-const GiftPoint = [{TotalPoint:100,LeftPoint:50}]
 const GiftOption = [
     {name:'pick',costpoint:'5'},
     {name:'Elixir 包膜弦',costpoint:'50'},
     {name:'全館商品9折卷一張',costpoint:'20'},
-    {name:'小練團室1h使用卷',costpoint:'60'}
+    {name:'小練團室1h使用卷',costpoint:'60'},
+    {name:'小練團室1h使用卷',costpoint:'60'},
+    {name:'小練團室1h使用卷',costpoint:'60'},
 ]
 
-const renderPoint = (data) =>(
-<View>
-    <Text style={{}}>目前可用點數:{data.item.LeftPoint}</Text>
-</View>
+const renderPoint = (data) => (
+    <View>
+        <Text style={{}}>目前可用點數:{data.item.LeftPoint}</Text>
+    </View>
 );
 
 const renderGift = (data) =>(
-    <View>
-        <Text>需要點數：{data.item.costpoint},商品：{data.item.name}</Text>
+    <View style={styles.item}>
+        <Text style={styles.title}>商品：{data.item.name}</Text>
+        <Text style={styles.Content}>需要點數：{data.item.costpoint}</Text>
     </View>
 )
 
@@ -35,13 +37,13 @@ return(
 
 <View style={{ flex: 1, justifyContent: 'center'}}>
     <View style={{ flex : 1 ,flexDirection:'row'}}>
-        <View style={{flex :1,backgroundColor:'red'}}>
+        <View style={{flex :1,backgroundColor:'orange'}}>
             <Text>這裡放圖</Text>
             <View style={{flex:1,justifyContent:'flex-end',}}>
                 <Text style={{fontSize:20}}>點我開始抽獎</Text>
             </View>
         </View>
-        <View style={{flex:2 ,backgroundColor:'blue', justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{flex:2 ,backgroundColor:'darkblue', justifyContent: 'center', alignItems: 'center'}}>
             <Text>可用點數</Text>
             {/* <View style={{height:'50%',width:'100%',justifyContent:'center',alignItems:'center',backgroundColor:'skyblue'}}>
                 <FlatList
@@ -52,19 +54,55 @@ return(
 
         </View>
     </View>
-    <View style={{flex : 2,backgroundColor:'skyblue'}}>
+    <View style={{flex : 2,backgroundColor:'skyblue',flexDirection:'row'}}>
+
         <FlatList
-        data = {GiftOption}
-        renderItem = {renderGift}
-        ></FlatList>
+            data = {GiftOption}
+            renderItem = {renderGift}
+        />
     </View>
-
-
  </View>
-
 </Container>
 
 
 )
 
-}export default Gift_index;
+}
+const styles = StyleSheet.create({
+
+    container: {
+  
+      //backgroundColor: '#00bfff',
+  
+      flex: 1,
+  
+    
+    },
+  
+    item: {
+  
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      backgroundColor: '#00ffff',
+      width:100,
+      height:100,
+      padding: 10,
+      marginVertical: 10,
+      marginHorizontal: 16,
+  
+    },
+  
+    title: {
+  
+      fontSize: 15,
+  
+    },
+    Content:{
+     textAlign:'right',
+     backgroundColor:'red',
+     textDecorationLine:'underline',
+     fontSize:12
+    }
+  
+  });export default Gift_index;
