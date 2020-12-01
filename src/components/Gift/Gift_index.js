363a727d70bf2a28f660a62c80e3d9cf6dbe1e4b
RayLin9981 <a259108736@gmail.com>
2020-12-01 16:48:54 +0800

    Gift UI20%

--- src/components/Gift/Gift_index.js
+++ src/components/Gift/Gift_index.js
@@ -1,8 +1,9 @@
 import React,{useEffect,useState} from 'react'
-import {View,Text,FlatList,StyleSheet} from 'react-native'
-import { Container, Content, Thumbnail, Button, Icon } from 'native-base'
+import {View,Text,FlatList,StyleSheet,Alert,Image,ImageBackground } from 'react-native'
+import { Container,Right} from 'native-base'
 import Header from '../UI/Header'
 import Colors from '../../styles/Colors'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const GiftOption = [
     {name:'pick',costpoint:'5'},
@@ -14,17 +15,26 @@ const GiftOption = [
 ]
 
 const NUN_COLUMNS = 2
+const uri = 'https://facebook.github.io/react-native/docs/assets/favicon.png'
 
-const renderPoint = (data) => (
-    <View>
-        <Text style={{}}>目前可用點數:{data.item.LeftPoint}</Text>
-    </View>
-);
 
 const renderGift = (data) =>(
     <View style={styles.item}>
+
+    <TouchableOpacity style={styles.itemButton}>
+    <ImageBackground  resizeMode={"repeat"} style={styles.center} source={{ uri: 'http://lorempixel.com/1920/1920/cats' }}>
+
         <Text style={styles.title}>商品：{data.item.name}</Text>
-        <Text style={styles.Content}>需要點數：{data.item.costpoint}</Text>
+        <Text style={styles.Content}> 需要點數：{data.item.costpoint}</Text>
+          
+          <View style={styles.myButton_circle}>
+            <Text>兌換</Text>
+         
+    </View>
+    </ImageBackground>
+    
+    </TouchableOpacity>
+    
     </View>
 )
 
@@ -36,12 +46,14 @@ function Gift_index() {
 return(
 <Container>
     <Header title="點數兌換"/>
+    
 
 <View style={{ flex: 1, justifyContent: 'center'}}>
     <View style={{ flex : 1 ,flexDirection:'row'}}>
-        <View style={{flex :1,backgroundColor:'orange'}}>
-            <Text>這裡放圖</Text>
-            <View style={{flex:1,justifyContent:'flex-end',}}>
+        <View style={{flex :3,backgroundColor:'orange'}}>
+        <Image  resizeMode={"repeat"} style={[styles.center]} source={{ uri: 'http://lorempixel.com/output/cats-q-c-640-480-9.jpg' }}/>
+
+            <View style={{flex:1,justifyContent:'flex-end'}}>
                 <Text style={{fontSize:20}}>點我開始抽獎</Text>
             </View>
         </View>
@@ -56,11 +68,7 @@ return(
 
         </View>
     </View>
-    <View style={{flex : 2, flexDirection:'row'}}>
-    {/* <View style={{flex:10}}>
-        <Text >123</Text>
-        <Text >需要點數</Text>
-    </View> */}
+    <View style={{flex : 4, flexDirection:'row'}}>
         <FlatList
             numColumns={NUN_COLUMNS}
             data = {GiftOption}
@@ -81,23 +89,59 @@ const styles = StyleSheet.create({
       flex: 1,
     },  
     item: { 
-        flex: 1, 
+        flex:1, 
         height: 200,
-        padding: 8,
-        margin: 4, 
+        padding:0,
+        margin:4, 
         borderRadius: 12, 
         borderColor: '#ccc', 
-        borderWidth: 1, 
+        borderWidth: 1,
+        justifyContent:'flex-end',
+        alignItems:'center',
+        overflow:'hidden'
     },
     title: {
       fontSize: 15,
     },
     Content:{
-     textAlign:'right',
-     backgroundColor:'red',
+      backgroundColor:'red',
      textDecorationLine:'underline',
      fontSize:12
-    }
+    },
+      myButton_circle:{
+        justifyContent:'center',
+        alignItems:'center',
+        padding: 1,
+        height: 50,
+        width: 50,  //The Width must be the same as the height
+        borderRadius:100, //Then Make the Border Radius twice the size of width or Height   
+        backgroundColor:'rgb(195, 125, 198)',
+     
+      },
+      itemButton: { 
+        flex: 1, 
+        // height: 500,
+        // width: 175,
+        // margin: 4, 
+        // borderRadius: 12, 
+        borderColor: '#ccc', 
+        borderWidth: 0,
+        justifyContent:'flex-end',
+        alignItems:'flex-end', 
+        overflow:'hidden'
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        width:200,
+        height:300,
+        backgroundColor:'transparent'
+    },
+        
+        
+        
+
   });
   
   export default Gift_index;
