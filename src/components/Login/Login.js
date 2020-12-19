8050d11c41544f4440446a74b41b1224a0d40616
yoting <jbhbjjjj@gmail.com>
2020-12-19 16:34:45 +0800

    google button

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,57 +1,61 @@
-import { Body,Button, Container, Content, Header, Title } from 'native-base';
-import React,{useState,useEffect} from 'react';
+import { Body, Button, Container, Content, Header, Title } from 'native-base';
+import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import {View, Text,TextInput,StyleSheet,Image} from 'react-native';
+import { View, Text, TextInput, StyleSheet, Image } from 'react-native';
 import route from '../../constants/route';
 
-
-function Login(props){
-    const { login,message } = props
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
+function Login(props) {
+    const { login, message } = props
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const navigation = useNavigation();
-    return(
+
+    return (
         <Container>
             {/* <Header>
                 <Body>
                     <Title style={{fontFamily:'Arial Black',fontSize:30}}>HuiRent</Title>
                 </Body>
             </Header> */}
-        <Content>
-            <View style={styles.form}>
-                <Image source={require('../../../pic/music-notes.png')} style={{width:90,height:90,marginTop:-200}}/>
-                <Text style={{fontSize:50,fontWeight:'700',marginBottom:80}}>HUIRENT
+            <Content>
+                <View style={styles.form}>
+                    <Image source={require('../../../pic/music-notes.png')} style={{ width: 90, height: 90, marginTop: -200 }} />
+                    <Text style={{ fontSize: 50, fontWeight: '700', marginBottom: 80 }}>HUIRENT
 
                 </Text>
-                <TextInput
-                    placeholder="請輸入帳號(電子信箱)"
-                    style={styles.inputstyle}
-                    onChangeText={text=>setEmail(text)}
-                />
-                <TextInput
-                    placeholder="請輸入密碼"
-                    style={styles.inputstyle}
-                    secureTextEntry={true}
-                    onChangeText={text=>setPassword(text)}
-                />
-                {(message)?<Text style={styles.text}>未輸入帳號、密碼或輸入錯誤！</Text>:<Text> </Text>}
-                <Button
-                    block
-                    dark
-                    style={styles.button}
-                    onPress={()=>login(email, password)}
-                >
-                    <Text style={{color:"#fff"}}>登入</Text>
-                </Button>
-                <Button
-                    transparent
-                    style={{alignSelf:'center'}}
-                    onPress={()=>navigation.navigate(route.SignUp)}
-                >
-                    <Text style={{color:"#5ca4cc"}}>註冊</Text>
-            </Button>
-            </View>
-        </Content>
+                    <TextInput
+                        placeholder="請輸入帳號(電子信箱)"
+                        style={styles.inputstyle}
+                        onChangeText={text => setEmail(text)}
+                    />
+                    <TextInput
+                        placeholder="請輸入密碼"
+                        style={styles.inputstyle}
+                        secureTextEntry={true}
+                        onChangeText={text => setPassword(text)}
+                    />
+                    {(message) ? <Text style={styles.text}>未輸入帳號、密碼或輸入錯誤！</Text> : <Text> </Text>}
+                    <Button
+                        block
+                        dark
+                        style={styles.button}
+                        onPress={() => login(email, password)}
+                    >
+                        <Text style={{ color: "#fff" }}>登入</Text>
+                    </Button>
+                    <Button
+                        transparent
+                        style={{ alignSelf: 'center' }}
+                        onPress={() => navigation.navigate(route.SignUp)}
+                    >
+                        <Text style={{ color: "#5ca4cc" }}>註冊</Text>
+                    </Button>
+                    <Text style={{ color: "#D0D0D0", marginTop: -5, marginBottom: 10 }}>–––––––––––––––––––––––––––––––––––––––––––––––––</Text>
+                    <Button block style={styles.button} >
+                        <Text style={{ color: "#fff" }}>使用Google帳號登入</Text>
+                    </Button>
+                </View>
+            </Content>
         </Container>
     )
 }
@@ -63,8 +67,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         padding: 35,
-        marginTop:300
-      },
+        marginTop: 300
+    },
     inputstyle: {
         width: '100%',
         marginBottom: 15,
@@ -74,12 +78,12 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         fontSize: 20
     },
-    button:{
-        padding:10,
+    button: {
+        padding: 10,
     },
     text: {
-        color:"#eb4034",
-        marginBottom:12
+        color: "#eb4034",
+        marginBottom: 12
     }
 })
 
