d03d2c81557fe4f52339a4c3670c61dc9087d622
yoting <jbhbjjjj@gmail.com>
2020-12-21 16:22:05 +0800

    yt

--- src/components/UI/SwipeList.js
+++ src/components/UI/SwipeList.js
@@ -111,6 +111,7 @@ function SwipeList(props) {
         renderItem={renderItem}
         maxSwipeDistance={240}
         renderQuickActions={({index, item}) => QuickActions(index, item)}
+        shouldBounceOnMount={true}
         ItemSeparatorComponent={()=> <View style={styles.itemSeparator}/>}
         // contentContainerStyle={{ flexGrow: 1,backgroundColor: 'red'}} // 背景顏色
       />
