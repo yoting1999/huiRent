885ecb9f916a58693ccd8217daced4a3302f85cd
ChingHuiHui <sunny@3drens.com>
2020-12-30 09:59:18 +0800

    rewrite the action to get the api

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -7,34 +7,37 @@ import { setAuthInfo } from '../../store/actions/auth'
 
 function Home() {
 
-  const { People } = agent
+  // const { People } = agent
 
   const userInfo = useSelector(state=>state.authReducer.userInfo)
-  const [userData, setUserData] = useState(null)
+  // const [userData, setUserData] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
   const dispatch = useDispatch();
 
-  const getUser =async() => {
-    setIsLoading(true)
-    const uid = firebase.auth().currentUser.uid
-    try {
-      const res = await People.getPeople(uid)
-      const tempUserData = {...Object.values(res.data)[0],ordererid: Object.keys(res.data)[0]}
-      setUserData(tempUserData)
-      setIsLoading(false)
-    } catch(err) {
-      console.log('err', err)
-    }
-  }
+  // const getUser = async() => {
+  //   setIsLoading(true)
+  //   const uid = firebase.auth().currentUser.uid
+  //   try {
+  //     const res = await People.getPeople(uid)
+  //     const tempUserData = {...Object.values(res.data)[0],ordererid: Object.keys(res.data)[0]}
+  //     setUserData(tempUserData)
+  //     setIsLoading(false)
+  //   } catch(err) {
+  //     console.log('err', err)
+  //   }
+  // }
+
+  // useEffect(()=>{
+  //   getUser()
+  // }, [])
 
   useEffect(()=>{
-    getUser()
+    console.log('userInfo', userInfo)
+  }, [userInfo])
+
+  useEffect(()=>{
+    dispatch(setAuthInfo())
   }, [])
-
-  useEffect(()=>{
-    if(!userData) return
-    dispatch(setAuthInfo(userData))
-  }, [userData])
 
   return <Layout isLoading={isLoading} userInfo={userInfo}/>
 }
